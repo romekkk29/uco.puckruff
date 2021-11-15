7e61fdb8ce84c6a4e284904589ebed3f705a19ae
romekkk29 <romegomez29@gmail.com>
2021-11-15 02:20:33 -0300

    provando

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let imagen=document.querySelector(".imagen");
 
 
-setTimeout(function(){ imagen.style.opacity="0.8"; }, 3000);
+setTimeout(function(){ imagen.style.opacity="0.6"; }, 3000);
 
 let menuBar=document.querySelector(".menu-bar");
 let boton=document.querySelector(".boton");
